c0f85fa4930265585e6409b4eac10431b0f3e756
BrhayanLS <87352660+BrhayanLS@users.noreply.github.com>
2023-03-31 15:09:31 -0500

    Se crea la persistencia para la base de datos en Railway

--- target/generated-sources/annotations/entities/ActualizacionPedido_.java
+++ target/generated-sources/annotations/entities/ActualizacionPedido_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-30T22:58:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-31T14:57:32")
 @StaticMetamodel(ActualizacionPedido.class)
 public class ActualizacionPedido_ { 
 
--- target/generated-sources/annotations/entities/ProductosProduccion_.java
+++ target/generated-sources/annotations/entities/ProductosProduccion_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-30T22:58:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-31T14:57:32")
 @StaticMetamodel(ProductosProduccion.class)
 public class ProductosProduccion_ { 
 
